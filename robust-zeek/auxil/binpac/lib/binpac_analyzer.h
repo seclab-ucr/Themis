3a01208af58e8c33778db708d51247125039c4d2
Zhongjie Wang <wzj401@gmail.com>
2021-06-23 12:26:47 -0700

    upload missing files due to incorrect .gitignore

---
+++ robust-zeek/auxil/binpac/lib/binpac_analyzer.h
@@ -0,0 +1,27 @@
+#ifndef binpac_an_h
+#define binpac_an_h
+
+namespace binpac {
+
+// TODO: Add the Done() function
+
+// The interface for a connection analyzer
+class ConnectionAnalyzer {
+public:
+	virtual ~ConnectionAnalyzer() {}
+	virtual void NewData(bool is_orig,
+	                     const unsigned char* begin_of_data,
+	                     const unsigned char* end_of_data) = 0;
+};
+
+// The interface for a flow analyzer
+class FlowAnalyzer {
+public:
+	virtual ~FlowAnalyzer() {}
+	virtual void NewData(const unsigned char* begin_of_data,
+	                     const unsigned char* end_of_data) = 0;
+};
+
+}  // namespace binpac
+
+#endif  // binpac_an_h
